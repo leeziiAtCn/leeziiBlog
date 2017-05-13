a4cf383cd27a00bdc0db91d4cc8e438ec307abaf
leezii <932265970@qq.com>
2017-05-14 00:11:24 +0800

    service user

---
+++ service/conf/port.js
@@ -0,0 +1,6 @@
+/**
+ * Created by think on 2017/5/13.
+ */
+module.exports = {
+    port: 8899
+}
---
+++ service/model/user.js
@@ -0,0 +1,66 @@
+/**
+ * Created by think on 2017/5/13.
+ */
+const Sequelize = require("sequelize")
+const sqlOpt = require("./../conf/mysql")
+const uuid = require("node-uuid")
+let sequelize = new Sequelize('myblog', sqlOpt.name, sqlOpt.pwd, {
+    host: sqlOpt.host,
+    dialect: 'mysql',
+    port: sqlOpt.port,
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 10000
+    }
+});
+let user = sequelize.define(
+    'user',
+    {
+        user_name: Sequelize.STRING,
+        user_pwd: Sequelize.STRING,
+        user_auth: Sequelize.INTEGER,
+        user_id: {
+            type: Sequelize.STRING,
+            primaryKey: true
+        }
+    }, {
+        timestamps: false,
+        freezeTableName: true
+    })
+
+module.exports = {
+    search(params){
+        return user.findAll({
+            where: {
+                user_name: params.name,
+                user_pwd: params.pwd
+            }
+        })
+    },
+    add(params){
+        return user.create({
+            user_name: params.name,
+            user_pwd: params.pwd,
+            user_id: uuid.v4(),
+            user_auth: 2
+        })
+    },
+    deleteUser(params){
+        return user.destroy({
+            where: {
+                user_name: params.name
+            }
+        });
+    },
+    update(params){
+        return user.update(
+            {
+                user_pwd: params.newPwd
+            }, {
+                where: {
+                    user_name: params.name
+                }
+            })
+    }
+}
