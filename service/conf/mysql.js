324c99dade8b0a6695659be736ed54d5d3f89301
leezii <leezii.atsh@outlook.com>
2018-01-18 22:58:50 +0800

    use php and change header img

--- service/conf/mysql.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by think on 2017/5/13.
- */
-module.exports = {
-    host:'118.89.195.97',
-    name:'leezii',
-    pwd:'147258',
-    port:'3306'
-}
--- service/conf/port.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by think on 2017/5/13.
- */
-module.exports = {
-    port: 8899
-}
