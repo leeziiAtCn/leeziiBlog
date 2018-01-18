2b28e20ebdf456435968933508e96b8d765f77eb
leezii <leezii.atsh@outlook.com>
2018-01-18 23:06:28 +0800

    fix header img url

--- usr/themes/Minimalism/sidebar.php
+++ usr/themes/Minimalism/sidebar.php
@@ -6,7 +6,7 @@
         <div class="info-header" style="background-image:url('<?php _e($this->options->siteUrl().date('Ymd').'.jpg'); ?>');">
             <span class="info-header-img">
                 <a href="<?php $this->options->adminUrl(); ?>" target="_blank">
-                    <img src="<?php $this->options->themeUrl('img/header.jpg'); ?>">
+                    <img src="<?php $this->options->themeUrl('img/header.jpeg'); ?>">
                 </a>
             </span>
         </div>
